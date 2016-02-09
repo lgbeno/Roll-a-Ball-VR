1a02d5c7dbbdce228fde86db36d78482f0d18d02
Luke Beno <lgbeno@gmail.com>
2016-02-09 01:25:42 -0600

    Initial import of Unity project

---
+++ Assets/scripts/camera_controller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_controller : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position + offset; ;
+	}
+}
---
+++ Assets/scripts/player_controller.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class player_controller : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text WinText;
+
+    void Start () {
+            rb = GetComponent<Rigidbody>();
+            count = 0;
+            setCountText();
+            WinText.text = "";
+        }
+    void FixedUpdate () {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(speed*moveHorizontal, 0, speed*moveVertical);
+
+            rb.AddForce(movement);
+        }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+            if (count>=21)
+            {
+                WinText.text = "You won!";
+            }
+        }
+            
+    }
+    
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+}
+
+//Destroy(other.gameObject);
